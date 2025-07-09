ac155f5d79fc6447f7b730310b35eed50ae36414
ydongs <2111742756@qq.com>
2025-07-09 12:55:22 +0000

    Deploying to gh-pages from @ ydongs/ydongs.github.io@4bc8c5fe0bcf4c471ade7fdf9e29f33a56ef726e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -21,7 +21,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv/";
+            window.location.href = "/_pages/cv/";
           },
         },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
         
